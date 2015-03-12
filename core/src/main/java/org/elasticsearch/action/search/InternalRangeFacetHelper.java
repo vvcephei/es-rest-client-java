745c9f559687fc6a8e0c823aa8397f85434d6cb8
John Roesler <john.roesler@bazaarvoice.com>
2015-03-11 19:24:33 -0700

    working on search

--- core/src/main/java/org/elasticsearch/action/search/InternalRangeFacetHelper.java
+++ core/src/main/java/org/elasticsearch/action/search/InternalRangeFacetHelper.java
@@ -10,6 +10,8 @@ import java.util.Map;
 import static com.bazaarvoice.elasticsearch.client.core.util.MapFunctions.requireList;
 import static com.bazaarvoice.elasticsearch.client.core.util.MapFunctions.requireMap;
 import static org.elasticsearch.common.xcontent.support.XContentMapValues.nodeDoubleValue;
+import static org.elasticsearch.common.xcontent.support.XContentMapValues.nodeLongValue;
+import static org.elasticsearch.common.xcontent.support.XContentMapValues.nodeStringValue;
 
 public class InternalRangeFacetHelper {
     public static RangeFacet fromXContent(final String facetName, final Map<String, Object> facetMap) {
@@ -18,8 +20,12 @@ public class InternalRangeFacetHelper {
         int i = 0;
         for (Object rangeObj : ranges) {
             Map<String, Object> range = requireMap(rangeObj, String.class, Object.class);
-            nodeDoubleValue(range.get("from"), Double.POSITIVE_INFINITY);
-            
+            double from = nodeDoubleValue(range.get("from"), Double.POSITIVE_INFINITY);
+            String from_str = nodeStringValue(range.get("from_str"), null);
+            double to = nodeDoubleValue(range.get("to"), Double.POSITIVE_INFINITY);
+            String to_str = nodeStringValue(range.get("to_str"), null);
+            long count = nodeLongValue(range.get("count"));
+
         }
         return new InternalRangeFacet()
     }
